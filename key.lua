57c47bebafb0ede345338676d3a1d8ad50153a38
VernsDEV <rapaafk@gmail.com>
2026-03-28 17:24:45 +0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,23 +1,24 @@
 -- ======================================================
 --   VOLTYX.GG | KEY DATABASE
---   Edit this file to add/remove/expire keys.
---   Format: { key = "...", expire = "YYYY-MM-DD" }
---   Set expire = "NEVER" for lifetime keys.
+--   Format: { key = "...", userId = number, expire = "YYYY-MM-DD" }
 -- ======================================================
 
 return {
     {
         key    = "VERNS-A1B2-C3D4-XXXX",
+        userId = 9220215943,
         expire = "2026-12-31",
         note   = "Key #1",
     },
     {
         key    = "VERNS-E5F6-G7H8-XXXX",
+        userId = nil, -- isi nanti
         expire = "2025-12-31",
         note   = "Key #2",
     },
     {
         key    = "VERNS-I9J0-K1L2-XXXX",
+        userId = nil, -- isi nanti
         expire = "2025-12-31",
         note   = "Key #3",
     },
