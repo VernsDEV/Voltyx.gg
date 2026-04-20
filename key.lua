5393c359b31a180733496c97b3cde4742909a0e2
VernsDEV <rapaafk@gmail.com>
2026-04-20 21:46:14 +0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -5,19 +5,19 @@
 
 return {
     {
-        key    = "VERNS-A1B2-C3D4-XXXX",
+        key    = "VERNS-A1B2-C3D4-PREMIUM",
         userId = nil, -- Diubah jadi nil biar universal
         expire = "2026-12-31",
         note   = "Key #1",
     },
     {
-        key    = "VERNS-E5F6-G7H8-KONTOL",
+        key    = "VERNS-E5F6-G7H8-PREMIUM",
         userId = nil, -- Diubah jadi nil biar universal
         expire = "2026-03-29",
         note   = "Key #2",
     },
     {
-        key    = "VERNS-GOD",
+        key    = "V3RTEX-POLIG-FREE",
         userId = nil, 
         expire = "2026-12-31",
         note   = "Key #3",
