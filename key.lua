b49f63b3b0d4d5bc7637921d12993971a5179bfd
VernsDEV <rapaafk@gmail.com>
2026-04-20 21:28:32 +0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,24 +1,24 @@
 -- ======================================================
 --   VOLTYX.GG | KEY DATABASE
---   Format: { key = "...", userId = number, expire = "YYYY-MM-DD" }
+--   Format: { key = "...", userId = nil, expire = "YYYY-MM-DD" }
 -- ======================================================
 
 return {
     {
         key    = "VERNS-A1B2-C3D4-XXXX",
-        userId = 9220215943,
+        userId = nil, -- Diubah jadi nil biar universal
         expire = "2026-12-31",
         note   = "Key #1",
     },
     {
         key    = "VERNS-E5F6-G7H8-KONTOL",
-        userId = 10082113567, -- isi nanti
+        userId = nil, -- Diubah jadi nil biar universal
         expire = "2026-03-29",
         note   = "Key #2",
     },
     {
         key    = "VERNS-GOD",
-        userId = nil, -- isi nanti
+        userId = nil, 
         expire = "2026-12-31",
         note   = "Key #3",
     },
